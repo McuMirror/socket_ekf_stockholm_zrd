faa74874aca2f085be99c242946c97ea560b9c5f
Fedor <lfa1@yandex.ru>
2026-06-26 18:39:15 +0300

    Update app_reporting.c

--- src/app_reporting.c
+++ src/app_reporting.c
@@ -2,6 +2,8 @@
 
 void app_forcedReport(uint8_t endpoint, uint16_t cluster_id, uint16_t attr_id) {
 
+    status_t st = ZCL_STA_SUCCESS;
+
     APP_DEBUG(UART_PRINTF_MODE, "app_forcedReport(). endpoint: %d, cluster_id: 0x%04x, attr_id: 0x%04x\r\n", endpoint, cluster_id, attr_id);
 
     if(zb_isDeviceJoinedNwk()) {
@@ -17,7 +19,7 @@ void app_forcedReport(uint8_t endpoint, uint16_t cluster_id, uint16_t attr_id) {
         zclAttrInfo_t *pAttrEntry = zcl_findAttribute(endpoint, cluster_id, attr_id);
 
         if (pAttrEntry) {
-            status_t st = zcl_sendReportCmd(endpoint, &dstEpInfo,  TRUE, ZCL_FRAME_SERVER_CLIENT_DIR,
+            st = zcl_sendReportCmd(endpoint, &dstEpInfo,  TRUE, ZCL_FRAME_SERVER_CLIENT_DIR,
                         cluster_id, pAttrEntry->id, pAttrEntry->type, pAttrEntry->data);
 
             APP_DEBUG(UART_PRINTF_MODE, "st from zcl_sendReportCmd: %d\r\n", st);
